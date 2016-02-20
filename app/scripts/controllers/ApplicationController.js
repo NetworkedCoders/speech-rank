84d675cfed25f31315af07fbdc132a2eb5c8d508
Marcin Kunysz <marcin.kunysz@fingo.pl>
2016-02-20 11:11:39 +0100

    app struct

---
+++ app/scripts/controllers/ApplicationController.js
@@ -0,0 +1,7 @@
+(function () {
+    'use strict';
+
+    angular.module('speechRank').controller('ApplicationController', function( $scope ) {
+        console.log('ApplicationController loaded..');
+    });
+})();
