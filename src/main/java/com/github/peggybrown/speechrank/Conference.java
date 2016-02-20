0296805e60e332f6727dbb956c9f02e6b30fd944
PeggyBrown <ola.waliczek@gmail.com>
2016-02-20 15:49:33 +0100

    Serve json data on endponts

---
+++ src/main/java/com/github/peggybrown/speechrank/Conference.java
@@ -0,0 +1,11 @@
+package com.github.peggybrown.speechrank;
+
+
+import java.util.List;
+
+public class Conference {
+
+    Long id;
+    String name;
+    List<Presentation> presentations;
+}
---
+++ src/main/java/com/github/peggybrown/speechrank/Presentation.java
@@ -0,0 +1,15 @@
+package com.github.peggybrown.speechrank;
+
+
+import java.util.List;
+
+public class Presentation {
+
+    Long id;
+    String title;
+    String link;
+    Float rate;
+    List<Comment> comments;
+
+
+}
