f2d20390f956c6a0f0b15fa6a43485c40b41bd19
Andrzej Bednarz <abednarz@networkedassets.org>
2016-02-21 11:57:16 +0100

    first version of import task

---
+++ src/test/java/ImporterTest.java
@@ -0,0 +1,126 @@
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.ResourceId;
+import com.google.api.services.youtube.model.SearchListResponse;
+import com.google.api.services.youtube.model.SearchResult;
+import com.google.api.services.youtube.model.Thumbnail;
+
+public class ImporterTest {
+
+
+	private static final long NUMBER_OF_VIDEOS_RETURNED = 25;
+
+	/**
+	 * Define a global instance of a Youtube object, which will be used
+	 * to make YouTube Data API requests.
+	 */
+	private static YouTube youtube;
+
+
+	/**
+	 * Define a global instance of the HTTP transport.
+	 */
+	public static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+
+	/**
+	 * Define a global instance of the JSON factory.
+	 */
+	public static final JsonFactory JSON_FACTORY = new JacksonFactory();
+
+	@Test
+	public void testImport() {
+
+		try {
+			// This object is used to make YouTube Data API requests. The last
+			// argument is required, but since we don't need anything
+			// initialized when the HttpRequest is initialized, we override
+			// the interface and provide a no-op function.
+			youtube = new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, new HttpRequestInitializer() {
+				public void initialize(HttpRequest request) throws IOException {
+				}
+			}).setApplicationName("andbed-1227").build();
+
+			// Define the API request for retrieving search results.
+			YouTube.Search.List search = youtube.search().list("id,snippet");
+
+			// Set your developer key from the {{ Google Cloud Console }} for
+			// non-authenticated requests. See:
+			// {{ https://cloud.google.com/console }}
+			String apiKey = "";
+			search.setKey(apiKey);
+			search.setQ("2015");
+
+			// Restrict the search results to only include videos. See:
+			// https://developers.google.com/youtube/v3/docs/search/list#type
+			search.setType("video");
+
+			// To increase efficiency, only retrieve the fields that the
+			// application uses.
+			search.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/default/url)");
+			search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED);
+
+			// Call the API and print results.
+			SearchListResponse searchResponse = search.execute();
+			List<SearchResult> searchResultList = searchResponse.getItems();
+			if (searchResultList != null) {
+				prettyPrint(searchResultList.iterator(), "2015");
+			}
+		} catch (GoogleJsonResponseException e) {
+			System.err.println("There was a service error: " + e.getDetails().getCode() + " : "
+					+ e.getDetails().getMessage());
+		} catch (IOException e) {
+			System.err.println("There was an IO error: " + e.getCause() + " : " + e.getMessage());
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+	}
+
+	/*
+ * Prints out all results in the Iterator. For each result, print the
+ * title, video ID, and thumbnail.
+ *
+ * @param iteratorSearchResults Iterator of SearchResults to print
+ *
+ * @param query Search query (String)
+ */
+	private static void prettyPrint(Iterator<SearchResult> iteratorSearchResults, String query) {
+
+		System.out.println("\n=============================================================");
+		System.out.println(
+				"   First " + NUMBER_OF_VIDEOS_RETURNED + " videos for search on \"" + query + "\".");
+		System.out.println("=============================================================\n");
+
+		if (!iteratorSearchResults.hasNext()) {
+			System.out.println(" There aren't any results for your query.");
+		}
+
+		while (iteratorSearchResults.hasNext()) {
+
+			SearchResult singleVideo = iteratorSearchResults.next();
+			ResourceId rId = singleVideo.getId();
+
+			// Confirm that the result represents a video. Otherwise, the
+			// item will not contain a video ID.
+			if (rId.getKind().equals("youtube#video")) {
+				Thumbnail thumbnail = singleVideo.getSnippet().getThumbnails().getDefault();
+
+				System.out.println(" Video Id" + rId.getVideoId());
+				System.out.println(" Title: " + singleVideo.getSnippet().getTitle());
+				System.out.println(" Thumbnail: " + thumbnail.getUrl());
+				System.out.println("\n-------------------------------------------------------------\n");
+			}
+		}
+	}
+}
