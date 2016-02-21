68249bcb837ca3678edfe46ae09b35f6064b2968
PeggyBrown <ola.waliczek@gmail.com>
2016-02-21 15:05:48 +0100

    preparation to real data

--- src/main/java/com/github/peggybrown/speechrank/Storage.java
+++ src/main/java/com/github/peggybrown/speechrank/Storage.java
@@ -1,31 +1,62 @@
 package com.github.peggybrown.speechrank;
 
+
+import com.google.common.collect.Maps;
 import javaslang.collection.List;
 import lombok.extern.java.Log;
 
+import java.util.Map;
+
 @Log
 public class Storage {
-    private List<Conference> conferences = List.empty();
+    private List<Conference> conferences;
+    private List<Year> years;
+
+    public Storage() {
+        conferences = List.empty();
+        years = List.empty();
+    }
 
     public void add(Rate rate) {
         log.info("Rate added");
-        conferences.toStream()
+        Presentation presentation = conferences
                 .flatMap(Conference::getPresentations)
-                .find(prez -> prez.getId().equals(rate.getPresentationId()))
-                .map(prez -> prez.addRate(rate));
+                .find(prez -> prez.getId().equals(rate.getPresentationId())).get();
+        presentation.addRate(rate);
     }
 
     public void add(Comment comment) {
         log.info("Comment added");
-        conferences.toStream()
+        conferences
                 .flatMap(Conference::getPresentations)
                 .find(prez -> prez.getId().equals(comment.getPresentationId()))
                 .map(prez -> prez.addComment(comment));
 
     }
 
-    public void add(Conference conf) {
+    public void add(String year, Conference conf) {
         log.info("Conference added" + conf.toString());
-        conferences.push(conf);
+        conferences = conferences.append(conf);
+        years.filter(y -> y.getYear().equals(year))
+                .map(y -> y.addConference(conf));
+
+    }
+
+    public java.util.List<Conference> getConferences() {
+        return conferences.toJavaList();
+
+    }
+
+    public List<Year> getYears() {
+        return years;
+    }
+
+    public Conference getConference(String id) {
+        //TODO what to return whe conference not found?
+        return conferences.find(conf -> conf.getId().equals(id)).get();
+    }
+
+    public void addYear(String year) {
+        years = years.append(new Year(year));
     }
 }
