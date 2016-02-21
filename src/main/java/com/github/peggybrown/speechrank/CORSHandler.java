ba10ff40f31b17f55250be05d1f1dac0476d7d9c
Andrzej Bednarz <abednarz@networkedassets.org>
2016-02-21 14:04:01 +0100

    cors handler added

---
+++ src/main/java/com/github/peggybrown/speechrank/CORSHandler.java
@@ -0,0 +1,21 @@
+package com.github.peggybrown.speechrank;
+
+import lombok.extern.slf4j.Slf4j;
+
+import ratpack.handling.Context;
+import ratpack.handling.Handler;
+import ratpack.http.MutableHeaders;
+
+@Slf4j
+public class CORSHandler implements Handler {
+
+	@Override public void handle(final Context ctx) throws Exception {
+		log.info("Adding CORS headers");
+		final MutableHeaders headers = ctx.getResponse().getHeaders();
+		headers.add("Access-Control-Allow-Origin", "*");
+		headers.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+		headers.add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+		ctx.next();
+	}
+
+}
