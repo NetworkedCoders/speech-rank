5f30d0840a4c193793ab7592106b7ac4bdce88e2
PeggyBrown <ola.waliczek@gmail.com>
2017-03-26 22:36:44 +0200

    Fixed test

--- src/test/java/ImporterTest.java
+++ src/test/java/ImporterTest.java
@@ -1,12 +1,18 @@
+import com.typesafe.config.ConfigFactory;
 import org.junit.Test;
 
 import com.github.peggybrown.speechrank.gateway.Importer;
 
+import java.io.File;
+
 public class ImporterTest {
+
+    String apiKey = ConfigFactory.parseFile(new File("api.conf"))
+        .getString("youtube.apiKey");
 
     @Test
     public void testImport() {
-        new Importer().importConfitura2015().forEach(System.out::println);
+        new Importer(apiKey).importConfitura2015().forEach(System.out::println);
     }
 
 }
