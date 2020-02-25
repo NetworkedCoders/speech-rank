83b69a7c87194de2ef6ea6775f1cedca7ace0200
PrzemysławFormella <przemyslaw.formella.praca@gmail.com>
2020-02-25 07:53:30 +0100

    Add okhttp3 library
    Add first API Test

---
+++ src/test/java/com/github/speechrank/rest/GetConferences.java
@@ -0,0 +1,35 @@
+package com.github.speechrank.rest;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class GetConferences {
+
+    @Test
+    public void Conferences() throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+            .build();
+        Request request = new Request.Builder()
+            .url("http://localhost:5050/api/conferences")
+            .method("GET", null)
+            .build();
+        Response response = client.newCall(request).execute();
+
+        //check is content-type is application/json
+        assertEquals("application/json", response.header("content-type"));
+
+        //check if status code is 200
+        assertEquals(200, response.code());
+
+        //check responseBody
+        String responseBody = response.body().string();
+        assertEquals("[{\"year\":\"2019\",\"conferences\":[{\"id\":\"12\",\"name\":\"DevConf\",\"presentations\":25},{\"id\":\"31\",\"name\":\"Boiling Frogs\",\"presentations\":25},{\"id\":\"41\",\"name\":\"Scalar\",\"presentations\":16},{\"id\":\"51\",\"name\":\"Confitura\",\"presentations\":25}]},{\"year\":\"2018\",\"conferences\":[{\"id\":\"21\",\"name\":\"Boiling Frogs\",\"presentations\":25},{\"id\":\"51\",\"name\":\"Confitura\",\"presentations\":25}]},{\"year\":\"2017\",\"conferences\":[{\"id\":\"11\",\"name\":\"DevConf\",\"presentations\":25}]}]", responseBody);
+        //TODO: powyzsza asercje napewno da sie ulepszyc, ale jeszcze nie wiem jak :)
+    }
+}
