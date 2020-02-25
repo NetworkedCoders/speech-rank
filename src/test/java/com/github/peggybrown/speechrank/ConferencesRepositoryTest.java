edcc76bc4f5230ae9bc02b6e5a3b47b9b0e1d482
Jan Sowa <jan.piotr.sowa@gmail.com>
2020-02-25 21:18:28 +0100

    Add ConferencesRepositoryTest with empty functions: -contains list of TODOs

---
+++ src/test/java/com/github/peggybrown/speechrank/ConferencesRepositoryTest.java
@@ -0,0 +1,26 @@
+package com.github.peggybrown.speechrank;
+
+import org.junit.Test;
+
+public class ConferencesRepositoryTest {
+
+    @Test
+    public void importAllConferencesShouldAddAnyConference() {
+        //TODO: check if after importAllConferences() the repository contains any conference
+    }
+
+    @Test
+    public void addAndGetConference() {
+        //TODO: check if after importConference(ConferenceImportDto conf) the repository contains this conference
+    }
+
+    @Test
+    public void addRateToPresentationTest() {
+        //TODO: check if after addRate(Rate rate) the presentation has this rate
+    }
+
+    @Test
+    public void addCommentToPresentationTest() {
+        //TODO: check if after addComment(Comment comment) the presentation has this comment
+    }
+}
