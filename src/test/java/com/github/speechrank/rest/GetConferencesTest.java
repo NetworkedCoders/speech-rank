43193422ca0fd366759868e1758b9ebb4a345b33
PrzemysławFormella <przemyslaw.formella.praca@gmail.com>
2020-02-26 12:43:29 +0100

    travis test24

--- src/test/java/com/github/speechrank/rest/GetConferencesTest.java
+++ src/test/java/com/github/speechrank/rest/GetConferencesTest.java
@@ -16,7 +16,7 @@ public class GetConferencesTest {
         OkHttpClient client = new OkHttpClient().newBuilder()
             .build();
         Request request = new Request.Builder()
-            .url("http://localhost:5050/api/conferences")
+            .url("https://76ea5e2c-c86f-4a27-baf2-c1667b6a1f35.mock.pstmn.io/api/conferences")
 //            .url("$VALIDATOR_URL/api/conferences")
             .method("GET", null)
             .build();
