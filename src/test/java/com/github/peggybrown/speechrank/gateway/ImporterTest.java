2160a9ed12b9a5c53f173c5a7cf9808d171b9065
BarteKKmita <bartekkmitaa@gmail.com>
2020-02-28 11:26:58 +0100

    Change Importer test case

--- src/test/java/com/github/peggybrown/speechrank/gateway/ImporterTest.java
+++ src/test/java/com/github/peggybrown/speechrank/gateway/ImporterTest.java
@@ -1,18 +1,12 @@
 package com.github.peggybrown.speechrank.gateway;
 
-import com.typesafe.config.ConfigFactory;
 import org.junit.Test;
-
-import java.io.File;
 
 public class ImporterTest {
 
-    String apiKey = ConfigFactory.parseFile(new File("api.conf"))
-        .getString("youtube.apiKey");
-
     @Test
-    public void testImport() {
-        new Importer(apiKey).importBoilingFrogs2019().forEach(System.out::println);
+    public void testImport(){
+        new Importer().importBoilingFrogs2019().forEach(System.out::println);
     }
 
 }
