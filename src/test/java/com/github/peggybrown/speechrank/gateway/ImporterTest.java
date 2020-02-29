4e49b93caccdff049b873b55069184279963d9e5
BarteKKmita <bartekkmitaa@gmail.com>
2020-02-29 14:34:03 +0100

    Restore formatting
    Restore years field

--- src/test/java/com/github/peggybrown/speechrank/gateway/ImporterTest.java
+++ src/test/java/com/github/peggybrown/speechrank/gateway/ImporterTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class ImporterTest {
 
     @Test
-    public void testImport(){
+    public void testImport() {
         new Importer().importBoilingFrogs2019().forEach(System.out::println);
     }
 
